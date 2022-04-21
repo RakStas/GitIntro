5f9db40700fa8219a0dc683a166fe57b17eee041
StanislavRak <rakstas51@Gmail.com>
2022-04-21 16:39:11 +0200

    thirs save code

--- src/GitHappyExmpl.java
+++ src/GitHappyExmpl.java
@@ -1,5 +1,5 @@
 public class GitHappyExmpl {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ src/OneMorePrgt.java
@@ -0,0 +1,5 @@
+public class OneMorePrgt {
+    public static void main(String[] args) {
+        System.out.println("Write some code");
+    }
+}
