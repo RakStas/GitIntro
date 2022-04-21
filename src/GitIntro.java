ca801562c8c3058f5785577f615a279bcafaa772
StanislavRak <rakstas51@Gmail.com>
2022-04-21 15:26:59 +0200

    Second

--- src/GitIntro.java
+++ src/GitIntro.java
@@ -1,6 +1,8 @@
 public class GitIntro {
     public static void main(String[] args) {
         System.out.println("First");
+        System.out.println("Second line");
         //dfghj
+        //next line
     }
 }
