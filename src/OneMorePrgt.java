d62cb7c84e50e4be52bf7abfd3bd5eed66a7e7ff
StanislavRak <rakstas51@Gmail.com>
2022-04-21 16:55:36 +0200

    lets try do it

--- src/OneMorePrgt.java
+++ src/OneMorePrgt.java
@@ -1,5 +1,9 @@
 public class OneMorePrgt {
     public static void main(String[] args) {
         System.out.println("Write some code");
+        System.out.println("Write some code again, second time");
+
+
+
     }
 }
