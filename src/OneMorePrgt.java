940f0439f6d1dc8c615f4bfb240b51ff7aa278df
StanislavRak <rakstas51@Gmail.com>
2022-04-21 16:57:03 +0200

    maiby

--- src/OneMorePrgt.java
+++ src/OneMorePrgt.java
@@ -2,8 +2,10 @@ public class OneMorePrgt {
     public static void main(String[] args) {
         System.out.println("Write some code");
         System.out.println("Write some code again, second time");
-
-
+        System.out.println( "Next trying to write with git");
+        int digit =5;
+        int num=5;
+        System.out.println(digit+num);
 
     }
 }
