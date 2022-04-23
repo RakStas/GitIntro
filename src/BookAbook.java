63abc8558fc2886859b0472c6179f1cdfbfcebf1
RakStas <96776686+RakStas@users.noreply.github.com>
2022-04-23 11:58:20 +0200

    Update BookAbook.java

--- src/BookAbook.java
+++ src/BookAbook.java
@@ -2,5 +2,6 @@ public class BookAbook {
     public static void main(String[] args) {
         System.out.println("Next line to the Git or GitHub");
         System.out.println("Lets try again to push on site GitHub");
+        //add from gitHub
     }
 }
