7dc9e6182652336762e1bc36dd49fb487cb365e4
StanislavRak <rakstas51@Gmail.com>
2022-04-23 11:39:19 +0200

    chekink site

--- src/BookAbook.java
+++ src/BookAbook.java
@@ -1,5 +1,6 @@
 public class BookAbook {
     public static void main(String[] args) {
         System.out.println("Next line to the Git or GitHub");
+        System.out.println("Lets try again to push on site GitHub");
     }
 }
